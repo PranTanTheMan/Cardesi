6479e02b4c0c5068ad43093fdd3153d083c27b00
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2023-01-08 15:51:28 -0600

    Delete Package

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,10 +13,7 @@ import {
   Stack,
   Link,
 } from "@chakra-ui/react";
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
-AOS.refresh();
+
 <script
   src="//code.tidio.co/ew045s7f91tlkp4hws9xam6fmbbzw1eb.js"
   async
@@ -82,12 +79,14 @@ export default function Home() {
           color={"white.500"}
           mb="3rem"
         >
-          <Heading fontSize={"3xl"}>How does Caravan make prices affordable?</Heading>
+          <Heading fontSize={"3xl"}>
+            How does Caravan make prices affordable?
+          </Heading>
           <Text py={"15px"}>
             Caravan makes prices affordable because we only buy used cars for
-            the right price. We don&apos;t buy cars that are overpriced and we don&apos;t
-            sell cars that are overpriced. We are here to help you find your
-            next dream car.
+            the right price. We don&apos;t buy cars that are overpriced and we
+            don&apos;t sell cars that are overpriced. We are here to help you
+            find your next dream car.
           </Text>
         </Box>
         <Box
@@ -105,125 +104,120 @@ export default function Home() {
           color={"textColor"}
           mb="3rem"
         >
-          <Heading fontSize={"3xl"}>Why can your team help me find the right car?</Heading>
+          <Heading fontSize={"3xl"}>
+            Why can your team help me find the right car?
+          </Heading>
           <Text py={"15px"}>
-            Caravan&apos;s team can help you find the right car because we are a team of passionate people who love cars. We want you to have the best car you&apos;ve ever had
+            Caravan&apos;s team can help you find the right car because we are a
+            team of passionate people who love cars. We want you to have the
+            best car you&apos;ve ever had
           </Text>
         </Box>
       </Flex>
       <Flex
-
-  _dark={{
-    bg: "#3e3e3e",
-  }}
-  p={50}
-  w="full"
-  alignItems="center"
-  justifyContent="center"
->
-  <Box
-  borderRadius={"20px"}
-     bg="blue.100"
-    _dark={{
-      bg: "gray.800",
-    }}
-  >
-    <Box
-      maxW="1xl"
-      w={{
-        md: "2xl",
-        lg: "2xl",
-      }}
-      mx="auto"
-      py={{
-        base: 12,
-        lg: 16,
-      }}
-      px={{
-        base: 4,
-        lg: 8,
-      }}
-      display={{
-        lg: "flex",
-      }}
-      alignItems={{
-        lg: "center",
-      }}
-      justifyContent={{
-        lg: "space-between",
-      }}
-    >
-      <chakra.h2
-        fontSize={{
-          base: "3xl",
-          sm: "4xl",
-        }}
-        fontWeight="extrabold"
-        letterSpacing="tight"
-        lineHeight="shorter"
-        color="gray.700"
         _dark={{
-          color: "gray.100",
+          bg: "#3e3e3e",
         }}
+        p={50}
+        w="full"
+        alignItems="center"
+        justifyContent="center"
       >
-        <chakra.span display="block">Ready to find your </chakra.span>
-        <chakra.span
-          display="block"
-      
-        >
-         next dream car?
-        </chakra.span>
-      </chakra.h2>
-      <Stack
-        direction={{
-          base: "column",
-          sm: "row",
-        }}
-        mt={{
-          base: 8,
-          lg: 0,
-        }}
-        flexShrink={{
-          lg: 0,
-        }}
-      >
-        <Link
-        href="./browse"
-          w={["full", , "auto"]}
-          display="inline-flex"
-          alignItems="center"
-          justifyContent="center"
-          px={5}
-          py={3}
-          border="solid transparent"
-          fontWeight="bold"
-          rounded="md"
-          shadow="md"
-          _light={{
-            color: "white",
-          }}
-          bg="blue.600"
+        <Box
+          borderRadius={"20px"}
+          bg="blue.100"
           _dark={{
-            bg: "blue.500",
-          }}
-          _hover={{
-            bg: "blue.700",
-            _dark: {
-              bg: "blue.600",
-            },
+            bg: "gray.800",
           }}
         >
-          Browse Now
-        </Link>
-      
-      </Stack>
-    </Box>
-  </Box>
-</Flex>
+          <Box
+            maxW="1xl"
+            w={{
+              md: "2xl",
+              lg: "2xl",
+            }}
+            mx="auto"
+            py={{
+              base: 12,
+              lg: 16,
+            }}
+            px={{
+              base: 4,
+              lg: 8,
+            }}
+            display={{
+              lg: "flex",
+            }}
+            alignItems={{
+              lg: "center",
+            }}
+            justifyContent={{
+              lg: "space-between",
+            }}
+          >
+            <chakra.h2
+              fontSize={{
+                base: "3xl",
+                sm: "4xl",
+              }}
+              fontWeight="extrabold"
+              letterSpacing="tight"
+              lineHeight="shorter"
+              color="gray.700"
+              _dark={{
+                color: "gray.100",
+              }}
+            >
+              <chakra.span display="block">Ready to find your </chakra.span>
+              <chakra.span display="block">next dream car?</chakra.span>
+            </chakra.h2>
+            <Stack
+              direction={{
+                base: "column",
+                sm: "row",
+              }}
+              mt={{
+                base: 8,
+                lg: 0,
+              }}
+              flexShrink={{
+                lg: 0,
+              }}
+            >
+              <Link
+                href="./browse"
+                w={["full", , "auto"]}
+                display="inline-flex"
+                alignItems="center"
+                justifyContent="center"
+                px={5}
+                py={3}
+                border="solid transparent"
+                fontWeight="bold"
+                rounded="md"
+                shadow="md"
+                _light={{
+                  color: "white",
+                }}
+                bg="blue.600"
+                _dark={{
+                  bg: "blue.500",
+                }}
+                _hover={{
+                  bg: "blue.700",
+                  _dark: {
+                    bg: "blue.600",
+                  },
+                }}
+              >
+                Browse Now
+              </Link>
+            </Stack>
+          </Box>
+        </Box>
+      </Flex>
 
       <Footer></Footer>
-      <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-      <script>AOS.init();</script>
     </>
   );
 }
