947bb1103a1dd30d9127aa0face536e7e6732a3a
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2023-01-08 20:20:21 -0600

    More cars

--- pages/browse.jsx
+++ pages/browse.jsx
@@ -40,7 +40,7 @@ export default function Browse() {
           </Flex>
         </Box>
       </Flex>
-      <Flex px="2rem" direction={"column"} marginTop={"20px"}>
+      <Flex px="2rem" direction={"column"} mt={"40px"} mb={"20px"}>
         <Heading pb="10px" color={"primary"} margin="auto">
           Browse our inventory
         </Heading>
@@ -74,8 +74,8 @@ export default function Browse() {
           price="10,900"
           miles="150,429"
         />
-        <CarCard 
-        make="Cadillac"
+        <CarCard
+          make="Cadillac"
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7oPR5bxI43ec07fjjPaQAV8ntY9oYxqrwGg&usqp=CAU"
           name="Cadillac Escalade"
           year="2017"
@@ -85,42 +85,67 @@ export default function Browse() {
           smallDescription="With a 6.2-liter V8 under the hood that delivers 420 hp and 460 lb-ft of torque, the car is very powerful."
           bigDescription="For 2017 the Cadillac Escalade has made minor changes to the content of its trim levels and also renamed them. New features include a teen-driver monitoring system, a rear passenger reminder (so you don't leave your kids in a hot car), an automated system for parking, and an innovative rearview mirror that incorporates a camera view."
         />
-        <CarCard 
-        make="Nissan"
-        image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
-      
-        name="Nissan Versa"
-        year="2011"
-        price="8,090"
-        miles="130,310"
-        condition="Good"
-        smallDescription="The Nissan Versa is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
-        bigDescription="The redesigned Versa sits 2.3 inches lower, is 1.8 inches wider and has a 1.6-inch-longer profile than the previous generation. Its styling bears a strong resemblance to its larger sibling, the Nissan Altima, which is a big step up for the subcompact. And while the price has increased, you're getting more features than before. Formerly top-trim-exclusive features such as power windows, power locks, and keyless entry with push-button start are now standard on the base S trim level."
+        <CarCard
+          make="Nissan"
+          image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
+          name="Nissan Versa"
+          year="2011"
+          price="8,090"
+          miles="130,310"
+          condition="Good"
+          smallDescription="The Nissan Versa is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
+          bigDescription="The redesigned Versa sits 2.3 inches lower, is 1.8 inches wider and has a 1.6-inch-longer profile than the previous generation. Its styling bears a strong resemblance to its larger sibling, the Nissan Altima, which is a big step up for the subcompact. And while the price has increased, you're getting more features than before. Formerly top-trim-exclusive features such as power windows, power locks, and keyless entry with push-button start are now standard on the base S trim level."
         />
         <CarCard
-
-        make="Toyota"
-        image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
-        name="Toyota Camry"
-        year="2017"
-        price="15,900"
-        miles="60,000"
-        condition="Excellent"
-        smallDescription="The Toyota Camry is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
-        bigDescription="The redesigned Camry sits 2.3 inches lower, is 1.8 inches wider and has a 1.6-inch-longer profile than the previous generation. Its styling bears a strong resemblance to its larger sibling, the Toyota Seqoia, which is a big step up for the subcompact. And while the price has increased, you're getting more features than before. Formerly top-trim-exclusive features such as power windows, power locks, and keyless entry with push-button start are now standard on the base S trim level."
+          make="Toyota"
+          image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
+          name="Toyota Camry"
+          year="2017"
+          price="15,900"
+          miles="60,000"
+          condition="Excellent"
+          smallDescription="The Toyota Camry is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
+          bigDescription="The redesigned Camry sits 2.3 inches lower, is 1.8 inches wider and has a 1.6-inch-longer profile than the previous generation. Its styling bears a strong resemblance to its larger sibling, the Toyota Seqoia, which is a big step up for the subcompact. And while the price has increased, you're getting more features than before. Formerly top-trim-exclusive features such as power windows, power locks, and keyless entry with push-button start are now standard on the base S trim level."
         />
         <CarCard
-        make="Honda"
-        image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
-        name="Honda Accord"
-        year="2018"
-        price="18,900"
-        miles="40,000"
-        condition="Excellent"
-        smallDescription="The Honda Accord is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
-        bigDescription={"The hybrid powertrain has become a more significant part of the Accord lineup for this new generation as it's standard on all but the two least-expensive trims. Those two, the LX and EX, are powered by the familiar 192-hp, 1.5-liter turbocharged four-cylinder gas engine carried over from the outgoing model—and they come exclusively with a continuously variable automatic transmission (CVT). "}
-       />
-       
+          make="Honda"
+          image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
+          name="Honda Accord"
+          year="2018"
+          price="18,900"
+          miles="40,000"
+          condition="Excellent"
+          smallDescription="The Honda Accord is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
+          bigDescription={
+            "The hybrid powertrain has become a more significant part of the Accord lineup for this new generation as it's standard on all but the two least-expensive trims. Those two, the LX and EX, are powered by the familiar 192-hp, 1.5-liter turbocharged four-cylinder gas engine carried over from the outgoing model—and they come exclusively with a continuously variable automatic transmission (CVT). "
+          }
+        />
+        <CarCard
+          make="Toyota"
+          image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
+          name="Toyota Corolla"
+          year="2019"
+          price="20,900"
+          miles="20,000"
+          condition="Excellent"
+          smallDescription="The Toyota Corolla is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
+          bigDescription={
+            "The hybrid powertrain has become a more significant part of the Corolla lineup for this new generation as it's standard on all but the two least-expensive trims. Those two, the LX and EX, are powered by the familiar 192-hp, 1.5-liter turbocharged four-cylinder gas engine carried over from the outgoing model—and they come exclusively with a continuously variable automatic transmission (CVT). "
+          }
+        />
+        <CarCard
+          make="Honda"
+          image="https://www.cnet.com/a/img/resize/84775954b9a9ec929c7252c08392ae799254e9f7/hub/2012/01/30/a03e0cd3-bb76-11e2-8a8e-0291187978f3/35128655_OVR_900x600.jpg?auto=webp&width=768"
+          name="Honda Civic"
+          year="2019"
+          price="20,900"
+          miles="20,000"
+          condition="Excellent"
+          smallDescription="The Honda Civic is a well-built small sedan, with plenty of space inside and enough standard technology and safety."
+          bigDescription={
+            "The hybrid powertrain has become a more significant part of the Civic lineup for this new generation as it's standard on all but the two least-expensive trims. Those two, the LX and EX, are powered by the familiar 192-hp, 1.5-liter turbocharged four-cylinder gas engine carried over from the outgoing model—and they come exclusively with a continuously variable automatic transmission (CVT). "
+          }
+        />
       </Wrap>
       <Footer></Footer>
     </>
